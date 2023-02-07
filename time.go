a06951716ec0310760ec7a045c37e361fd69fb93
chagridsada <chagridsada@dollarsign.co>
2023-02-07 21:16:36 +0700

    refactor(time): rename time struct name

--- time.go
+++ time.go
@@ -2,27 +2,27 @@ package utils
 
 import "time"
 
-var Time TimeStruct
+var Time TimeUtil
 var BangkokTimeLocation, _ = time.LoadLocation("Asia/Bangkok")
 
-type TimeStruct struct{}
+type TimeUtil struct{}
 
-func (TimeStruct) ParseInBangkokLocation(layout, value string) (time.Time, error) {
+func (TimeUtil) ParseInBangkokLocation(layout, value string) (time.Time, error) {
 	return time.ParseInLocation(layout, value, BangkokTimeLocation)
 }
 
-func (TimeStruct) InBangkokTime(value time.Time) time.Time {
+func (TimeUtil) InBangkokTime(value time.Time) time.Time {
 	return value.In(BangkokTimeLocation)
 }
 
-func (TimeStruct) ToMySQLDateTime(value time.Time) string {
+func (TimeUtil) ToMySQLDateTime(value time.Time) string {
 	return value.Format("2006-01-02 15:04:05")
 }
 
-func (TimeStruct) ToMySQLDate(value time.Time) string {
+func (TimeUtil) ToMySQLDate(value time.Time) string {
 	return value.Format("2006-01-02")
 }
 
-func (TimeStruct) ToMySQLTime(value time.Time) string {
+func (TimeUtil) ToMySQLTime(value time.Time) string {
 	return value.Format("15:04:05")
 }
