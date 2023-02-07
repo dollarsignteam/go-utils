06902fe775c5e127fb73944c5cffac9d6846a25d
chagridsada <chagridsada@dollarsign.co>
2023-02-07 21:48:44 +0700

    feat(utils): add `PackageName`, `PointerOf` func

--- utils.go
+++ utils.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"runtime"
+	"strings"
+)
+
+func PointerOf[T any](v T) *T {
+	return &v
+}
+
+func PackageName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	parts := strings.Split(runtime.FuncForPC(pc).Name(), "/")
+	pkg := strings.Split(parts[len(parts)-1], ".")
+	return pkg[0]
+}
--- utils_test.go
+++ utils_test.go
@@ -1,1 +1,20 @@
 package utils_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dollarsignteam/go-utils"
+)
+
+func TestPointerOf(t *testing.T) {
+	value := 0
+	result := utils.PointerOf(value)
+	assert.Equal(t, &value, result)
+}
+
+func TestPackageName(t *testing.T) {
+	name := utils.PackageName()
+	assert.Equal(t, "go-utils_test", name)
+}
