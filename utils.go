14c7a8fdaa7c60757ad2cbb3119f31c98b7e5732
chagridsada <chagridsada@dollarsign.co>
2023-02-08 14:01:20 +0700

    feat(utils): add `UniqueOf` function

--- utils.go
+++ utils.go
@@ -19,3 +19,15 @@ func PackageName() string {
 	pkg := strings.Split(parts[len(parts)-1], ".")
 	return pkg[0]
 }
+
+func UniqueOf[T any](input []T) []T {
+	u := make([]T, 0, len(input))
+	m := make(map[any]struct{})
+	for _, v := range input {
+		if _, ok := m[v]; !ok {
+			m[v] = struct{}{}
+			u = append(u, v)
+		}
+	}
+	return u
+}
--- utils_test.go
+++ utils_test.go
@@ -18,3 +18,33 @@ func TestPackageName(t *testing.T) {
 	name := utils.PackageName()
 	assert.Equal(t, "go-utils_test", name)
 }
+
+func TestUniqueOf(t *testing.T) {
+	tests := []struct {
+		input    []interface{}
+		expected []interface{}
+	}{
+		{
+			input:    []interface{}{1, 2, 3, 2, 1},
+			expected: []interface{}{1, 2, 3},
+		},
+		{
+			input:    []interface{}{"foo", "bar", "baz", "foo"},
+			expected: []interface{}{"foo", "bar", "baz"},
+		},
+		{
+			input:    []interface{}{1, "foo", true, 2, "foo"},
+			expected: []interface{}{1, "foo", true, 2},
+		},
+	}
+	for _, test := range tests {
+		result := utils.UniqueOf(test.input)
+		assert.Equal(t, test.expected, result)
+	}
+}
+
+func BenchmarkUniqueOf(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		utils.UniqueOf([]interface{}{1, "foo", true, 2, "foo"})
+	}
+}
