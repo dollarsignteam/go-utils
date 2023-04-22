80f4e924a8ff68c4e607a16a184fdfb90cc218ef
chagridsada <chagridsada@dollarsign.co>
2023-04-22 23:29:41 +0700

    refactor(echo_jwt): update echo jwt util

--- echo_jwt.go
+++ echo_jwt.go
@@ -1,9 +1,7 @@
 package utils
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -14,104 +12,78 @@ import (
 var EchoJWT EchoJWTUtil
 
 type EchoJWTUtil struct {
-	signingKey    []byte
-	expiresTTL    time.Duration
+	config        EchoJWTConfig
 	echoJWTConfig echojwt.Config
 }
 
-type EchoJWTConfig struct {
-	SigningKey string
-	ExpiresTTL time.Duration
+type JWTToken struct {
+	SignedString string
+	Claims       jwt.RegisteredClaims
 }
 
-type JWTToken struct {
-	ID     string
-	Token  string
-	Claims jwt.RegisteredClaims
+type EchoJWTConfig struct {
+	SigningKey        string
+	ExpiresTTL        time.Duration
+	BeforeSuccessFunc func(token *jwt.Token, c echo.Context) error
 }
 
 func (EchoJWTUtil) New(config EchoJWTConfig) *EchoJWTUtil {
-	signingKey := []byte(config.SigningKey)
-	return &EchoJWTUtil{
-		signingKey: signingKey,
-		expiresTTL: config.ExpiresTTL,
+	echoJWTUtil := &EchoJWTUtil{
+		config: config,
 		echoJWTConfig: echojwt.Config{
-			SigningKey: signingKey,
+			SigningKey: []byte(config.SigningKey),
 		},
 	}
+	echoJWTUtil.echoJWTConfig.ParseTokenFunc = echoJWTUtil.ParseTokenFunc
+	return echoJWTUtil
+}
+
+func (eJWT EchoJWTUtil) CreateToken(claims jwt.RegisteredClaims) JWTToken {
+	if claims.ID == "" {
+		claims.ID = String.UUID()
+	}
+	if claims.IssuedAt == nil {
+		claims.IssuedAt = jwt.NewNumericDate(time.Now())
+	}
+	if claims.ExpiresAt == nil {
+		claims.ExpiresAt = jwt.NewNumericDate(time.Now().Add(eJWT.config.ExpiresTTL))
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, _ := token.SignedString([]byte(eJWT.config.SigningKey))
+	return JWTToken{
+		SignedString: signedToken,
+		Claims:       claims,
+	}
+}
+
+func (eJWT EchoJWTUtil) KeyFunc(token *jwt.Token) (any, error) {
+	if token.Method.Alg() != jwt.SigningMethodHS256.Name {
+		return nil, fmt.Errorf("unexpected jwt signing method=%v", token.Header["alg"])
+	}
+	return []byte(eJWT.config.SigningKey), nil
+}
+
+func (eJWT EchoJWTUtil) ParseToken(signedToken string) (*jwt.Token, error) {
+	token, err := jwt.ParseWithClaims(signedToken, &jwt.RegisteredClaims{}, eJWT.KeyFunc)
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func (eJWT EchoJWTUtil) ParseTokenFunc(c echo.Context, auth string) (any, error) {
+	token, err := eJWT.ParseToken(auth)
+	if err != nil {
+		return nil, err
+	}
+	if eJWT.config.BeforeSuccessFunc != nil {
+		if err := eJWT.config.BeforeSuccessFunc(token, c); err != nil {
+			return nil, err
+		}
+	}
+	return token, nil
 }
 
 func (eJWT EchoJWTUtil) JWTAuth() echo.MiddlewareFunc {
 	return echojwt.WithConfig(eJWT.echoJWTConfig)
-}
-
-func (eJWT EchoJWTUtil) CreateToken(claims jwt.RegisteredClaims) (JWTToken, error) {
-	if claims.ID == "" {
-		claims.ID = String.UUID()
-	}
-	timeNow := time.Now()
-	if claims.IssuedAt == nil {
-		claims.IssuedAt = jwt.NewNumericDate(timeNow)
-	}
-	if claims.ExpiresAt == nil {
-		claims.ExpiresAt = jwt.NewNumericDate(timeNow.Add(eJWT.expiresTTL))
-	}
-	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	token, err := jwtToken.SignedString(eJWT.signingKey)
-	if err != nil {
-		return JWTToken{}, err
-	}
-	return JWTToken{
-		ID:     claims.ID,
-		Token:  token,
-		Claims: claims,
-	}, nil
-}
-
-func (eJWT EchoJWTUtil) ParseToken(tokenString string) (jwt.MapClaims, error) {
-	// Parse the token and check for validity and expiration
-	token, err := jwt.ParseWithClaims(tokenString, jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("invalid signing method")
-		}
-		return eJWT.signingKey, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// Check if the token is still valid
-	if !token.Valid {
-		return nil, errors.New("invalid token")
-	}
-
-	// Get the claims from the token
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		return nil, errors.New("invalid claims")
-	}
-
-	return claims, nil
-}
-
-func (eJWT EchoJWTUtil) GetSession(c echo.Context) (jwt.MapClaims, error) {
-	// Get the token from the Authorization header
-	authHeader := c.Request().Header.Get("Authorization")
-	if authHeader == "" {
-		return nil, errors.New("missing Authorization header")
-	}
-
-	// Split the token from the "Bearer " prefix
-	auth := strings.Split(authHeader, "Bearer ")
-	if len(auth) != 2 {
-		return nil, errors.New("invalid Authorization header format")
-	}
-
-	// Parse the token and return the claims
-	claims, err := eJWT.ParseToken(auth[1])
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse token: %v", err)
-	}
-
-	return claims, nil
 }
---
+++ echo_jwt_test.go
@@ -0,0 +1,115 @@
+package utils_test
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/dollarsignteam/go-utils"
+)
+
+var testEchoJWTConfig = utils.EchoJWTConfig{
+	SigningKey: "my-secret-key",
+	ExpiresTTL: time.Hour,
+}
+var testClaims = jwt.RegisteredClaims{
+	Subject: "test-user",
+}
+
+func TestCreateToken(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	token := echoJWT.CreateToken(testClaims)
+	assert.NotEmpty(t, token.SignedString)
+	assert.Equal(t, testClaims.Subject, token.Claims.Subject)
+}
+
+func TestKeyFunc(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, testClaims)
+	result, err := echoJWT.KeyFunc(token)
+	assert.Nil(t, err)
+	assert.Equal(t, []byte(testEchoJWTConfig.SigningKey), result)
+}
+
+func TestKeyFunc_Error(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	token := jwt.NewWithClaims(jwt.SigningMethodNone, testClaims)
+	result, err := echoJWT.KeyFunc(token)
+	assert.Nil(t, result)
+	assert.EqualError(t, err, "unexpected jwt signing method=none")
+}
+
+func TestParseToken(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, testClaims)
+	signedToken, _ := token.SignedString([]byte(testEchoJWTConfig.SigningKey))
+	result, err := echoJWT.ParseToken(signedToken)
+	assert.True(t, result.Valid)
+	assert.NoError(t, err)
+}
+
+func TestParseToken_InvalidSigningKey(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, testClaims)
+	signedToken, _ := token.SignedString([]byte("wrong-secret-key"))
+	_, err := echoJWT.ParseToken(signedToken)
+	assert.EqualError(t, err, "token signature is invalid: signature is invalid")
+}
+
+func TestParseTokenFunc(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	_, err := echoJWT.ParseTokenFunc(nil, "")
+	assert.EqualError(t, err, "token is malformed: token contains an invalid number of segments")
+}
+
+func TestParseTokenFunc_InvalidToken(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, testClaims)
+	signedToken, _ := token.SignedString([]byte(testEchoJWTConfig.SigningKey))
+	result, err := echoJWT.ParseTokenFunc(nil, signedToken)
+	assert.Nil(t, err)
+	assert.IsType(t, &jwt.Token{}, result)
+}
+
+func TestParseTokenFunc_BeforeSuccessFunc_Error(t *testing.T) {
+	var beforeSuccessCalled bool
+	config := utils.EchoJWTConfig{
+		SigningKey: testEchoJWTConfig.SigningKey,
+		BeforeSuccessFunc: func(token *jwt.Token, c echo.Context) error {
+			beforeSuccessCalled = true
+			return errors.New("mock error")
+		},
+	}
+	echoJWT := utils.EchoJWT.New(config)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, testClaims)
+	signedToken, _ := token.SignedString([]byte(testEchoJWTConfig.SigningKey))
+	result, err := echoJWT.ParseTokenFunc(nil, signedToken)
+	assert.Nil(t, result)
+	assert.EqualError(t, err, "mock error")
+	assert.True(t, beforeSuccessCalled)
+}
+
+func TestJWTAuth(t *testing.T) {
+	echoJWT := utils.EchoJWT.New(testEchoJWTConfig)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		token := c.Get("user").(*jwt.Token)
+		claims := token.Claims.(*jwt.RegisteredClaims)
+		return c.String(http.StatusOK, claims.Subject)
+	})
+	e.Use(echoJWT.JWTAuth())
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+	token := echoJWT.CreateToken(testClaims)
+	req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %s", token.SignedString))
+	e.ServeHTTP(res, req)
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, testClaims.Subject, res.Body.String())
+}
