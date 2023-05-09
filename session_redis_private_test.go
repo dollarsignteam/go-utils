63adbb77dd068512d26384209a3f359529502332
chagridsada <chagridsada@dollarsign.co>
2023-05-09 17:01:39 +0700

    test(session_redis): update private spec

--- session_redis_private_test.go
+++ session_redis_private_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/alicebob/miniredis/v2"
+	"github.com/alicebob/miniredis/v2/server"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,9 +36,34 @@ func TestSessionRedisHandler_deleteSessionKeys(t *testing.T) {
 	s, client := createMockRedisClient(t)
 	defer s.Close()
 	h := createHandler(client)
-	_ = s.Set("foo:bar", "baz")
-	err := h.deleteSessionKeys("foo:*")
-	assert.Nil(t, err)
-	found := s.Exists("foo:var")
-	assert.False(t, found)
+
+	t.Run("success", func(t *testing.T) {
+		_ = s.Set("foo:bar", "baz")
+		err := h.deleteSessionKeys("foo:*")
+		assert.Nil(t, err)
+		found := s.Exists("foo:var")
+		assert.False(t, found)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		s.Server().SetPreHook(func(p *server.Peer, s1 string, s2 ...string) bool {
+			p.WriteError("mock error")
+			return true
+		})
+		err := h.deleteSessionKeys("foo:*")
+		assert.EqualError(t, err, "mock error")
+	})
+
+	t.Run("pipe error", func(t *testing.T) {
+		t.Skip()
+		s.Server().SetPreHook(func(p *server.Peer, s1 string, s2 ...string) bool {
+			if s1 == "SCAN" {
+				return true
+			}
+			p.WriteError("mock error")
+			return true
+		})
+		err := h.deleteSessionKeys("foo:*")
+		assert.EqualError(t, err, "mock error")
+	})
 }
