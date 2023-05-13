35379de3671bd0c0df119516273d726846485ccb
chagridsada <chagridsada@dollarsign.co>
2023-05-13 23:24:34 +0700

    feat(image): add `IsImage` method

--- image.go
+++ image.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"io"
 	"mime/multipart"
+	"net/http"
 	"strings"
 )
 
@@ -11,8 +13,23 @@ var Image ImageUtil
 // ImageUtil is a utility struct for image related functions
 type ImageUtil struct{}
 
+// IsImageContentType checks if the content type is an image
+func (ImageUtil) IsImageContentType(fh *multipart.FileHeader) bool {
+	contentType := fh.Header.Get("Content-Type")
+	return strings.HasPrefix(contentType, "image/")
+}
+
 // IsImage checks if the file is an image
 func (ImageUtil) IsImage(fh *multipart.FileHeader) bool {
-	contentType := fh.Header.Get("Content-Type")
+	f, err := fh.Open()
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+	buf := make([]byte, 512)
+	if _, err = io.ReadFull(f, buf); err != nil && err != io.ErrUnexpectedEOF {
+		return false
+	}
+	contentType := http.DetectContentType(buf)
 	return strings.HasPrefix(contentType, "image/")
 }
--- image_test.go
+++ image_test.go
@@ -1,15 +1,19 @@
 package utils_test
 
 import (
+	"bytes"
 	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/dollarsignteam/go-utils"
 )
 
-func TestIsImage(t *testing.T) {
+func TestIsImageContentType(t *testing.T) {
 	tests := []struct {
 		name     string
 		fileType string
@@ -25,8 +29,67 @@ func TestIsImage(t *testing.T) {
 					"Content-Type": {test.fileType},
 				},
 			}
-			result := utils.Image.IsImage(fh)
+			result := utils.Image.IsImageContentType(fh)
 			assert.Equal(t, test.expected, result)
 		})
 	}
+}
+
+func TestIsImage(t *testing.T) {
+	tests := []struct {
+		name     string
+		filename string
+		content  []byte
+		expected bool
+	}{
+		{
+			name:     "valid jpg",
+			filename: "test.jpg",
+			content:  []byte("\xFF\xD8\xFF"),
+			expected: true,
+		},
+		{
+			name:     "valid png",
+			filename: "test.png",
+			content:  []byte("\x89PNG\x0D\x0A\x1A\x0A"),
+			expected: true,
+		},
+		{
+			name:     "invalid file header",
+			filename: "test.txt",
+			content:  []byte{0x00, 0x01, 0x02, 0x03},
+			expected: false,
+		},
+		{
+			name:     "empty file header",
+			filename: "test.txt",
+			content:  []byte{},
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			body := new(bytes.Buffer)
+			writer := multipart.NewWriter(body)
+			part, _ := writer.CreateFormFile("file", test.filename)
+			part.Write(test.content)
+			writer.Close()
+			req := httptest.NewRequest(http.MethodPost, "/", body)
+			req.Header.Set("Content-Type", writer.FormDataContentType())
+			c := echo.New().NewContext(req, nil)
+			fh, err := c.FormFile("file")
+			if assert.NoError(t, err) {
+				result := utils.Image.IsImage(fh)
+				assert.Equal(t, test.expected, result)
+			}
+		})
+	}
+}
+
+func TestIsImage_InvalidFile(t *testing.T) {
+	fh := &multipart.FileHeader{
+		Filename: "invalid-file.txt",
+	}
+	result := utils.Image.IsImage(fh)
+	assert.False(t, result)
 }
