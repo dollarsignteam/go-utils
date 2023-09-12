4a6291753985ffade26c65f7e317355c3092a9e2
chagridsada <chagridsada@dollarsign.co>
2023-09-13 01:07:52 +0700

    test(amqp): update amqp spec

--- amqp_test.go
+++ amqp_test.go
@@ -43,10 +43,31 @@ func (suite *AMQPTestSuite) TestSendToQueue() {
 	}
 	suite.NotNil(sender)
 	wg := sync.WaitGroup{}
+	count := 0
+	receiverListCount := 9
+	r, e := suite.amqpClient.NewReceiver(queueName)
+	suite.Nil(e)
+	rList, eList := suite.amqpClient.NewReceiverList(queueName, receiverListCount)
+	suite.Nil(eList)
+	suite.Len(rList, receiverListCount)
+	handlerFunc := func(message *amqp.Message, err error) *utils.AMQPMessageHandler {
+		suite.Nil(err)
+		if err != nil {
+			return &utils.AMQPMessageHandler{
+				Rejected: true,
+				IsClosed: false,
+			}
+		}
+		count++
+		defer wg.Done()
+		return nil
+	}
+	go suite.amqpClient.Received(r, handlerFunc)
+	go suite.amqpClient.ReceivedList(rList, handlerFunc)
+	suite.T().Log("Waiting for all messages to be received...")
 	for i := 0; i < messageCount; i++ {
 		wg.Add(1)
 		go func(index int) {
-			defer wg.Done()
 			id := fmt.Sprintf("index[%d]", index)
 			message := amqp.NewMessage([]byte(id))
 			message.Properties = &amqp.MessageProperties{
@@ -58,59 +79,50 @@ func (suite *AMQPTestSuite) TestSendToQueue() {
 	}
 	suite.T().Log("Waiting for all messages to be sent...")
 	wg.Wait()
-	count := 0
-	receiverListCount := 9
-	r, e := suite.amqpClient.NewReceiver(queueName)
-	suite.Nil(e)
-	rList, eList := suite.amqpClient.NewReceiverList(queueName, receiverListCount)
-	suite.Nil(eList)
-	suite.Len(rList, receiverListCount)
-	wg.Add(messageCount)
-	handlerFunc := func(message *amqp.Message, err error) *utils.AMQPMessageHandler {
-		if err != nil {
-			suite.T().Log("Error: ", err.Error())
-			return &utils.AMQPMessageHandler{
-				Rejected: true,
-				IsClosed: true,
-			}
-		}
-		count++
-		defer wg.Done()
-		return nil
-	}
-	go suite.amqpClient.Received(r, handlerFunc)
-	go suite.amqpClient.ReceivedList(rList, handlerFunc)
-	suite.T().Log("Waiting for all messages to be received...")
-	wg.Wait()
 	suite.Equal(messageCount, count)
 }
 
 func (suite *AMQPTestSuite) TestPublishToTopic() {
-	suite.T().Skip()
-	publisher, err := suite.amqpClient.NewPublisher("test-topic")
+	topicName := "test-topic"
+	messageCount := 1000
+	publisher, err := suite.amqpClient.NewPublisher(topicName)
 	if err != nil {
 		suite.FailNow("failed to create publisher", err)
 		return
 	}
 	suite.NotNil(publisher)
-	wg1 := sync.WaitGroup{}
-	for i := 0; i < 1000; i++ {
-		wg1.Add(1)
+	wg := sync.WaitGroup{}
+	count := 0
+	r, e := suite.amqpClient.NewSubscriber(topicName)
+	suite.Nil(e)
+	handlerFunc := func(message *amqp.Message, err error) *utils.AMQPMessageHandler {
+		suite.Nil(err)
+		if err != nil {
+			return &utils.AMQPMessageHandler{
+				Rejected: true,
+				IsClosed: false,
+			}
+		}
+		count++
+		defer wg.Done()
+		return &utils.AMQPMessageHandler{
+			Rejected: true,
+		}
+	}
+	go suite.amqpClient.Received(r, handlerFunc)
+	suite.T().Log("Waiting for all messages to be received...")
+	for i := 0; i < messageCount; i++ {
+		wg.Add(1)
 		go func(index int) {
-			defer wg1.Done()
 			id := fmt.Sprintf("index[%d]", index)
 			message := amqp.NewMessage([]byte(id))
-			message.Properties = &amqp.MessageProperties{
-				CorrelationID: id,
-			}
 			err := suite.amqpClient.Publish(publisher, message)
-			if err != nil {
-				suite.Nil(err)
-			}
+			suite.Nil(err)
 		}(i)
 	}
-	suite.T().Log("Waiting for all messages to be sent...")
-	wg1.Wait()
+	suite.T().Log("Waiting for all messages to be publish...")
+	wg.Wait()
+	suite.Equal(messageCount, count)
 }
 
 func TestIntegrationAMQPTestSuite(t *testing.T) {
